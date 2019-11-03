846b766c9eb62a580386f29aeec3ed031022f567
Josh W <jtwong10@gmail.com>
2019-11-03 12:53:12 -0800

    Update

--- Controllers/HomeController.js
+++ Controllers/HomeController.js
@@ -8,6 +8,17 @@ exports.Index = async function(request, response) {
 exports.MakeText = async function(request, response) {
   let userInput = await request.body.userinput;
   console.log(userInput);
+  var transformedInput = "";
 
-  response.render("Home/Index", { hi: userInput });
+  if (userInput) {
+    for (var i = 0; i < userInput.length; i++) {
+      if ((i + 1) % 2 > 0) {
+        transformedInput += userInput.charAt(i).toUpperCase();
+      } else {
+        transformedInput += userInput.charAt(i).toLowerCase();
+      }
+    }
+  }
+
+  response.render("Home/Index", { hi: transformedInput });
 };
